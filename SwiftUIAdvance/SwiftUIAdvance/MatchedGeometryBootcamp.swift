fd97769cca5385842c930a3ac6cee0d2deec94d3
Ankit Gupta <ankit.gupta.ext@sivantos.com>
2025-05-27 07:57:42 +0530

    MatchedGeometey

--- SwiftUIAdvance/SwiftUIAdvance/MatchedGeometryBootcamp.swift
+++ SwiftUIAdvance/SwiftUIAdvance/MatchedGeometryBootcamp.swift
@@ -3,8 +3,36 @@
 import SwiftUI
 
 struct MatchedGeometryBootcamp: View {
+  @State var selectedItem = ""
+  @Namespace var namespace
+  let items = ["Home", "Order", "Profile"]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack {
+        Spacer()
+        
+        HStack {
+          ForEach(items, id: \.self) { item in
+            ZStack {
+              if(selectedItem == item) {
+                RoundedRectangle(cornerRadius: 10)
+                  .fill(.orange.opacity(0.7))
+                  .matchedGeometryEffect(id: "selected_item", in: namespace)
+              }
+              
+              Text(item)
+                .bold()
+                .foregroundStyle(selectedItem == item ? .white : .primary)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 70)
+            .onTapGesture {
+              withAnimation(.spring.speed(2)) {
+                selectedItem = item
+              }
+            }
+          }
+        }
+        .padding()
+      }
     }
 }
 
